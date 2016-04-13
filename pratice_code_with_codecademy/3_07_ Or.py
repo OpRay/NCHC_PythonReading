5b2d932a97c30dcf5985b79cf6f1ee23158c12ae
OpRay <1203067@gmail.com>
2016-04-13 09:59:06 +0800

    Merge branch 'master' of https://github.com/OpRay/NCHC_PythonReading into HEAD
    # Conflicts:
    #	README.md

---
+++ pratice_code_with_codecademy/3_07_ Or.py
@@ -0,0 +1,25 @@
+# Or
+# The boolean operator or returns True when at least one expression on either side of or is true. For example:
+#
+# 1 < 2 or 2 > 3 is True;
+# 1 > 2 or 2 > 3 is False.
+
+# Instructions
+# Time to practice with or!
+#
+# Set bool_one equal to the result of 2**3 == 108 % 100 or 'Cleese' == 'King Arthur'
+# Set bool_two equal to the result of True or False
+# Set bool_three equal to the result of 100**0.5 >= 50 or False
+# Set bool_four equal to the result of True or True
+# Set bool_five equal to the result of 1**100 == 100**1 or 3 * 2 * 1 != 3 + 2 + 1
+
+# True or False = True
+bool_one = True
+
+bool_two = 2**3 == 108 % 100 or 'Cleese' == 'King Arthur'
+
+bool_three = False or 2 < 1
+
+bool_four = 1 < 2 or 2 < 1
+
+bool_five = False
---
+++ pratice_code_with_codecademy/3_08_Not.py
@@ -0,0 +1,26 @@
+# Not
+# The boolean operator not returns True for false statements and False for true statements.
+#
+# For example:
+#
+# not False will evaluate to True, while not 41 > 40 will return False.
+
+# Instructions
+# Let's get some practice with not.
+#
+# Set bool_one equal to the result of not True
+# Set bool_two equal to the result of not 3**4 < 4**3
+# Set bool_three equal to the result of not 10 % 3 <= 10 % 2
+# Set bool_four equal to the result of not 3**2 + 4**2 != 5**2
+# Set bool_five equal to the result of not not False
+
+bool_one = 41 < 40
+# not 81 < 64 is True
+bool_two = True
+# not 1 <= 0 is True
+bool_three = True
+# not 25 != 25 is True
+bool_four = True
+
+bool_five = 41 < 40
+
