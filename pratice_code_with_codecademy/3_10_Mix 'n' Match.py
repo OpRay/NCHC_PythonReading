94ef46bddb60830a8020260cc0f44b1e3aa1dbb9
OpRay <1203067@gmail.com>
2016-04-18 17:11:03 +0800

    Merge branch 'master' of https://github.com/OpRay/NCHC_PythonReading into HEAD
    # Conflicts:
    #	README.md

---
+++ pratice_code_with_codecademy/3_10_Mix 'n' Match.py
@@ -0,0 +1,27 @@
+# Mix 'n' Match
+# Great work! We're almost done with boolean operators.
+#
+# # Make me false
+# bool_one = (2 <= 2) and "Alpha" == "Bravo"
+
+# Instructions
+# This time we'll give the expected result, and you'll use some combination of boolean operators to achieve that result.
+#
+# Remember, the boolean operators are and, or, and not. Use each one at least once!
+
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two =  1 < 2 and "Ray" == "Ray"
+
+# Make me false!
+bool_three = 1 > 2 or 2 < 1
+
+# Make me true!
+bool_four = 1 < 2 or (not 1 > 2)
+
+# Make me true!
+bool_five = 1 < 2 and ((not 1 > 2) or 1 > 2)
